ef7b5a12673c52ba14002903ec27867227589465
Robby Dhindsa <dhindsa.1996@gmail.com>
2023-01-09 20:30:51 -0800

    Implemented assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const greenEmoji = String.fromCodePoint(0x12705);
+const redEmoji = String.fromCodePoint(0x1274C);
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(greenEmoji + greenEmoji + greenEmoji + "Assertion Passed: " + actual + " === " + expected);
+  } else
+    console.log(redEmoji + redEmoji + redEmoji + "Assertion Failed: " + actual + " !== " + expected);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("LHL", "LHL");
+assertEqual(6, 23);
