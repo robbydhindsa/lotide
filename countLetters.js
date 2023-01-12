06619691ceec35317f4b79ab8e57eb765adaa63b
Robby Dhindsa <dhindsa.1996@gmail.com>
2023-01-11 16:11:49 -0800

    Implemented countLetters function

---
+++ countLetters.js
@@ -0,0 +1,31 @@
+// assertEqual FUNCTION
+const greenEmoji = String.fromCodePoint(0x12705);
+const redEmoji = String.fromCodePoint(0x1274C);
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${greenEmoji}${greenEmoji}${greenEmoji} Assertion Passed: ${actual} === ${expected}`);
+  } else
+    console.log(`${redEmoji}${redEmoji}${redEmoji} Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// countLetters FUNCTION
+const countLetters = function(sentence) {
+  const results = {};
+  for (const letter of sentence) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  delete results[' '];
+  return results;
+}
+
+// TEST CODE
+console.log(countLetters("lighthouse in the house"));
+console.log(countLetters("LHL"));
+
+assertEqual(countLetters("lighthouse in the house")["h"], 4);
+assertEqual(countLetters("LHL")["L"], 2);
