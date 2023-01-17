c49f3b550eb301c3405dee9f2ebc57c30df2d92c
Robby Dhindsa <dhindsa.1996@gmail.com>
2023-01-16 18:27:58 -0800

    Refactored tail.js and added a test file for it

---
+++ test/tailTest.js
@@ -0,0 +1,19 @@
+const tail = require('../tail');
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+console.log(tail([5,6,7]));
+console.log(tail(["Hello", "Lighthouse", "Labs"]));
+console.log(tail([23]));
+console.log(tail([]));
+
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
